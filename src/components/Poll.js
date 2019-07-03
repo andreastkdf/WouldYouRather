f48ec52b7050c0476a5d0976ef709f1e5ba8842b
andreastkdf <andreaskostakis@gmail.com>
2019-07-03 21:02:16 +0100

    Poll UI

---
+++ src/components/Poll.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import PollCard from "./material/PollCard"
+import { formatPoll } from "../utils/helpers"
+
+class Poll extends Component {
+  render() {
+    const { poll, viewResults, authedUser } = this.props
+
+    if (poll === null) {
+      return <p>This poll doesn't exist</p>
+    }
+
+    return (
+      <PollCard poll={poll} authedUser={authedUser} summary={viewResults} />
+    )
+  }
+}
+
+function mapStateToProps({ polls, users, authedUser }, { id }) {
+  const poll = polls[id]
+
+  return {
+    poll: poll ? formatPoll(poll, users[poll.author]) : null,
+    viewResults: users[authedUser].answers[id] ? true : false,
+    authedUser
+  }
+}
+
+export default connect(mapStateToProps)(Poll)
